232b08e005f98c7abad7b568e2f11469d6e1813e
Mr.Tan <oncwnuKHgCucflJ5YDjM-GP5MZA8@git.weixin.qq.com>
2019-11-21 10:55:02 +0800

    1111

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,59 +1,46 @@
 <template>
-  <div class="home">
-    <!-- 顶部导航 start -->
-    <div class="top-banner">
-      <div class="activity">
-        <img src="https://img02.hua.com/pc/images/pc_newuser_order_100.png">
+  <Layout>
+    <Header>
+      <div class="top-banner">
+        <div class="activity">
+          <img src="https://img02.hua.com/pc/images/pc_newuser_order_100.png">
+        </div>
+        <nav class="top-nav">
+          <ul class="top-nav-left">
+            <li>
+              <a href="">
+                <span>收藏鲜花网(hua.com)</span>
+              </a>
+            </li>
+          </ul>
+          <ul class="top-nav-right">
+            <li></li>
+          </ul>
+        </nav>
       </div>
-      <nav class="top-nav">
-        <ul class="top-nav-left">
-          <li>
-            <a href="">
-              <span>收藏鲜花网(hua.com)</span>
-            </a>
-          </li>
-        </ul>
-        <ul class="top-nav-right">
-          <li></li>
-        </ul>
-      </nav>
-    </div>
-    <!-- 顶部导航 end -->
-    <!-- 头部 start -->
-    <header class="header">
-      <div class="logo"></div>
-      <div class="search"></div>
-      <div class="service"></div>
-    </header>
-    <!-- 头部 end -->
-    <!-- 功能导航 start -->
-    <nav class="content-nav"></nav>
-    <!-- 功能导航 end -->
-    <!-- 轮播图 start -->
-    <!-- 轮播图 end -->
-    <!-- 内容 start -->
-    <!-- 内容 end -->
-    <!-- 底部 start-->
-    <!-- 底部导航 start -->
-    <!-- 底部导航 end -->
-    <!-- 底部帮助 start -->
-    <!-- 底部帮助 end -->
-    <!-- 友情链接 start -->
-    <!-- 友情链接 end -->
-    <!-- 底部 end -->
-  </div>
+      <!-- 顶部导航 end -->
+      <!-- 头部 start -->
+      <div class="header">
+        <div class="logo"></div>
+        <div class="search"></div>
+        <div class="service"></div>
+      </div>
+      <!-- 头部 end -->
+    </Header>
+    <Layout>
+        <Sider hide-trigger>Sider</Sider>
+        <Content>Content</Content>
+    </Layout>
+    <Footer>Footer</Footer>
+  </Layout>
 </template>
-
+  
 <script type="text/ecmascript-6">
-export default {};
+  export default {
+  
+  }
 </script>
-
-<style lang="stylus">
-  .home
-    width 100%
-    .top-banner
-      .activity
-        img 
-          width 100%
-        
+  
+<style lang="stylus" rel="stylesheet/stylus">
+  
 </style>
