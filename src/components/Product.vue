fb6d8b094a1364cad7b5b4db3eb01a023bf647a2
kobe <594482966@qq.com>
2019-11-21 21:31:04 +0800

    pull

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,7 +2,7 @@
   <div>
     <img class="topImg" src="../image/top.png" alt="">
     <header>
-      <div class="nav">
+      <div id="nav">
         <ul class="leftNav">
           <li>
             <a href="###">
@@ -73,11 +73,145 @@
           
         </ul>
       </div>
-      <div class="search">
-
+     
+      <div id="search">
+        <img class="im" src="./imgs/1.jpg" alt="">
+        <div class="import">
+          <input type="text" placeholder="商品关键词"> 
+          <button>搜索</button>
+          <ul class="text">
+            <li>玫瑰花|</li> 
+            <li>永生花|</li>
+            <li>生日仙花|</li>
+            <li>金玫瑰|</li>
+          </ul>
+          <ul class="phone">
+            <li>
+              <i class="iconfont icon-dianhua"></i>
+              <span>400-889-8188</span>
+              <i class="iconfont icon-ziyuan"></i>
+               <a href="#">在线客服</a>  
+            </li>
+            <!-- <li style="color:#e1dde1;">|</li> --> 
+          </ul>
+        </div>
       </div>
-      
+
+      <div id="guide">
+        <div class="leftGuide">
+          <ul class="guideA">
+            <li>
+              <a href="##">全部商品导购</a>
+              <i class="iconfont icon-xiangxiajiantou"></i>
+            </li>
+          </ul>
+        </div>
+        <div class="rightGuide">
+          <ul class="guideB">
+            <li>
+              <a href="##">首页</a>
+            </li>
+            <li>
+              <a href="##">鲜花</a>
+            </li>
+            <li>
+              <a href="##">永生花</a>
+            </li>
+            <li>
+              <a href="##">蛋糕</a>
+            </li>
+            <li>
+              <a href="##">礼品</a>
+            </li>
+            <li>
+              <a href="##">巧克力</a>
+            </li>
+            <li>
+              <a href="##">花语大全</a>
+            </li>
+            <li>
+              <a href="##">设计师沈臻选鲜花</a>
+            </li>
+            <li>
+              <a href="##">感恩节鲜花礼物</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
+
+    <main>
+      <div class="loader">
+        <a href="##" style="font-weight:bold">首页</a>
+        <span>></span>
+        <a href="##">鲜花</a>
+        <span>></span>
+        <a href="##">一往情深</a>
+        <span>></span>
+      </div>
+      <div class="content">
+        <div class="leftContent">
+          <img style="width:510px;height:520px" src="./imgs/huaa.jpg" alt="">
+        </div>
+        <div class="rightContent">
+          <h1 style="font-size:20px;font-weight:bold">一往情深--精品玫瑰礼盒:19枝红玫瑰，勿忘我0.1扎</h1>
+          <h1 style="color:orange;font-size:20px;margin-top:10px">经典爆款，年销售冠军！</h1>
+          <h1 style="margin-top:10px">已售
+            <i style="color:orange">7.5</i>
+            万件
+          </h1>
+          <div class="cut"></div>
+          <p>类 别：鲜花-鲜花礼盒 编 号：9010966</p>
+          <p>材 料：高档礼盒装鲜花:19枝红玫瑰，勿忘我0.1扎</p>
+          <p>包 装：牛皮纸和深咖啡色条纹纸，银色缎带花结，魔力铁山灰包装盒</p>
+          <p>花 语：你的轻柔像阵微风，让我从容不迫，想让你知道，我对你始终一往情深。</p>
+          <p>配 送：限送100多个主要城市的市区及近郊：北京,上海,深圳,广州,成都,武汉,南京,杭州,苏州,天津,西安,长沙,东莞,厦门,佛山,沈阳,合肥,重庆,大连,郑州,青岛,太原,无锡,石家庄,济南,宁波,哈尔滨,乌鲁木齐,贵阳,昆明,福州,长春,南昌,兰州,珠海,南宁,中山,常州,金华,邯郸,泉州,海口,嘉兴,南通,呼和浩特,廊坊,唐山,温州,徐州,绵阳,烟台,襄阳,保定,潍坊,镇江,衡阳,包头,赣州,扬州,清远,荆州,莆田,汉中,洛阳,湛江,九江,鞍山,大庆,秦皇岛,张家口,桂林,吉林,淄博,蚌埠,柳州,遵义,邢台,宜春,漳州,三亚,宜宾,东营,临沂,德州,开封,大同,龙岩,齐齐哈尔,连云港,新乡,黄冈,焦作,十堰,驻马店,信阳,牡丹江,黄石,宝鸡,丹东,阜阳,北海,聊城,锦州,许昌,内江,萍乡,安庆,承德,商丘,盘锦,乐山,沧州,河源,营口,平顶山,临汾,韶关,日照,新余,晋城,松原,淮北,淮南,晋中,潮州,滨州,自贡,六安,株州,濮阳,常熟,晋江,顺德,江阴,吴江,昆山,义乌,惠阳,银川,温江,燕郊,新都,涿州,南沙,宜兴,即墨,海安县,都江堰,增城,仙桃,菏泽</p>
+          <p>配 送至：请选择
+            <i class="iconfont icon-xiangxiajiantou"></i>
+          </p>
+          <div class="cut" style="margin-top:65px"></div>
+          
+          <ul class="show">
+            <li>
+              <a href="##">
+                <img src="./imgs/small.jpg" alt="">
+                <span>一往情深</span>
+              </a> 
+            </li>
+            <li>
+              <a href="##">
+                <img src="./imgs/small.jpg" alt="">
+                <span>一往情深</span>
+              </a> 
+            </li>
+            <li>
+              <a href="##">
+                <img src="./imgs/small.jpg" alt="">
+                <span>一往情深</span>
+              </a> 
+            </li>
+            <li>
+              <a href="##">
+                <img src="./imgs/small.jpg" alt="">
+                <span>一往情深</span>
+              </a> 
+            </li>
+          </ul>
+
+          <div class="price"> 
+            <p>花礼价:
+              <span style="color:orange;font-weight:bold">$239</span>
+            </p>
+          </div>
+
+          
+
+        </div>
+      </div>
+    </main>
+      
+    
+
   </div>
 </template>
 
@@ -92,11 +226,11 @@
     height 80px
   header
     width 100%
-    height 35px
+    height 230px
     background #f2f2f2
-    .nav
+    #nav
       width 1400px
-      height 35px
+      height 40px
       // background pink 
       margin 0 auto
       .leftNav
@@ -115,27 +249,125 @@
         justify-content space-between
         line-height 35px
         padding 0 5px
-    .search
-      width 1200px
-      height 120px
+    #search
+      width 100%
+      height 140px
       background pink
-    
+      margin 0 auto 
+      position relative
+      .im
+        position absolute
+        top 30%
+        left 17%
+      .import
+        position absolute
+        top 45px
+        left 45% 
+        input   
+          width 270px
+          height 40px  
+        button 
+          width 80px
+          height 42px
+          background #ff6a00
+          font-size 18px
+          color #fff
+      .phone
+        position absolute
+        right -400px
+        top 8px
+        .server
+          margin-left 0
+          z-index 100
+      .text
+        display flex    
+    #guide
+      width 100%
+      height 50px
+      background yellow  
+      display flex
+      padding 0 300px
+      .leftGuide
+        width 200px
+        height 50px
+        background orange
+        margin-left 80px
+        .guideA
+          font-size 19px 
+          margin-left 12px
+          line-height 50px
+          a
+            color white
+          i 
+            margin-left 48px
+      .rightGuide
+        width 1200px
+        height 50px
+        background yellowgreen 
+        .guideB
+          display flex
+          justify-content space-around
+          line-height 50px
+          padding 0 30px
+          a
+            font-size 20px
+            color white
+  main 
+    .loader
+      width 1200px
+      height 20px 
+      background #99FF99
+      margin 0 auto
+      a
+        font-size 20px
+    .content
+      width 1200px
+      height 1000px
+      display flex
+      background #E8CCFF
+      margin 5px auto  0
+      border 1px solid #a4cf61
+      
+      .leftContent
+        width 520px
+        height 650px
+        margin 33px 0 0 73px
+        background 	#FF69B4 
+      .rightContent
+        width 560px
+        height 1000px
+        background #33FFDD
+        margin 33px 0 0 100px
+        .cut
+          width 500px
+          height 1px
+          background #737373
+          margin-top 30px
+          margin-bottom 30px
+        p
+          margin-top 15px
+          font-size 15px
+        .show
+          display flex 
+          li
+            margin-left 15px
+            img   
+              width 50px
+              height 50px
+            span 
+              margin-bottom 60px
+        .price
+          margin-top 60px      
 
+
+          
+        
         
 
-    
+
+
 
        
-    
-  
-
-
-
-
-
-
-
-
 </style>
 
  
