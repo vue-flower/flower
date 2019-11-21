8ac2a2f438efb7106465fadd6d939413d070abc1
Mr.Tan <oncwnuKHgCucflJ5YDjM-GP5MZA8@git.weixin.qq.com>
2019-11-21 10:46:06 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div >
-    kkkkkkkk
-    <router-link to="/home">首页</router-link>
-    <router-link to="/flower">鲜花</router-link>
-    <router-link to="/yongshenghua">永生花</router-link>
-    <router-link to="/product">详情</router-link>
+    <router-link to="/home"></router-link>
+    <router-link to="/flower"></router-link>
+    <router-link to="/yongshenghua"></router-link>
+    <router-link to="/product"></router-link>
     <router-view></router-view>
   </div>
 </template>
