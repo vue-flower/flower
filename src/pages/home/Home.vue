a1635b8cfe838606ab3dcd2e7e44039be816bc24
Mr.Tan <oncwnuKHgCucflJ5YDjM-GP5MZA8@git.weixin.qq.com>
2019-11-21 14:24:33 +0800

    list

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout class="home">
     <Header>
-        <div class="activity">
-          <img src="https://img02.hua.com/pc/images/pc_newuser_order_100.png">
-        </div>
+      <div class="activity">
+        <img src="https://img02.hua.com/pc/images/pc_newuser_order_100.png" />
+      </div>
 
       <div class="top-banner">
         <nav class="top-nav">
@@ -23,19 +23,18 @@
                 <span>花礼网app</span>
               </a>
             </li>
-
           </ul>
           <ul class="top-nav-right">
             <li>
               <a href="">
                 <span>您好,请登录</span>
               </a>
-            </li>            
+            </li>
             <li>
               <a href="">
                 <span>注册</span>
               </a>
-            </li> 
+            </li>
             <li>
               <a href="">
                 <span>订单查询</span>
@@ -45,7 +44,7 @@
               <a href="">
                 <span>我的花礼</span>
               </a>
-            </li> 
+            </li>
             <li>
               <a href="">
                 <span>客户服务</span>
@@ -68,65 +67,113 @@
       <div class="header">
         <div class="logo">
           <h1>
-            <img src="../../image/静态图片/home/ia_200000105.png" alt="">
+            <img src="../../image/静态图片/home/ia_200000105.png" alt="" />
           </h1>
         </div>
         <div class="search">
-          <input type="text" placeholder="商品关键字">
+          <input type="text" placeholder="商品关键字" />
           <button>搜索</button>
-          <Button type="primary" icon="ios-search">Search</Button>
         </div>
-        <div class="service"></div>
+        <div class="service">
+          <div>
+            <span>400-889-8188</span>
+          </div>
+          <div>
+            <span>在线客服</span>
+          </div>
+        </div>
       </div>
       <!-- 头部 end -->
     </Header>
     <Layout>
-        <Sider hide-trigger>Sider</Sider>
-        <Content>Content</Content>
+      <Sider hide-trigger> 
+        <div class="cate">
+
+        </div>
+      </Sider>
+      <Content>Content</Content>
     </Layout>
     <Footer>Footer</Footer>
   </Layout>
 </template> 
 <script>
-
-  export default {
-  
-  }
+export default {};
 </script>
   
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-  .home
-    .top-banner
-      width 100%
-      // height 116px
-      background #f2f2f2
-      .top-nav
-        height 35px
-        width 1200px
-        margin 0 auto 
-        line-height 35px
-        font-size 12px
-        display flex
-        justify-content space-between
-        .top-nav-left
-          width 348px
-          display flex
-          justify-content space-between
-          // background-color red
-        .top-nav-right
-          width 632px
-          display flex
-          justify-content space-between
-          // background-color yellow
-    .header
-      height 120px
-      width 1200px
-      margin 0 auto 
-      display flex
-      justify-content space-between
-      background-color pink
-      .logo
-        display flex
-        justify-content center
-        align-items center
+.home {
+  .top-banner {
+    width: 100%;
+    // height 116px
+    background: #f2f2f2;
+
+    .top-nav {
+      height: 35px;
+      width: 1200px;
+      margin: 0 auto;
+      line-height: 35px;
+      font-size: 12px;
+      display: flex;
+      justify-content: space-between;
+
+      .top-nav-left {
+        width: 348px;
+        display: flex;
+        justify-content: space-between;
+        // background-color red
+      }
+
+      .top-nav-right {
+        width: 632px;
+        display: flex;
+        justify-content: space-between;
+        // background-color yellow
+      }
+    }
+  }
+
+  .header {
+    height: 120px;
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    // background-color pink
+    .logo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .search {
+      height: 34px;
+
+      input {
+        width: 238px;
+        height: 32px;
+        // box-sizing border-box
+        border: 1px solid #f56000;
+      }
+
+      button {
+        width: 82px;
+        height: 34px;
+        background: #f56000;
+        font-size: 16px;
+        color: #fff;
+      }
+    }
+
+    .service {
+      width: 252px;
+      display: flex;
+      justify-content: space-between;
+      color: #737373;
+      font-size: 14px;
+      line-height: 20px;
+    }
+  }
+}
 </style>
