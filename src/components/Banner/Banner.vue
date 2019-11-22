f9df50331272ef8c2ad2b67c50646af3efdbbda9
Mr.Tan <oncwnuKHgCucflJ5YDjM-GP5MZA8@git.weixin.qq.com>
2019-11-22 16:40:52 +0800

    修改了banner轮播图

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(el, index) in arrItem" :key="index">
+            <img class="img" :src="el.imgUrl" />
+        </div>
+      </div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
+
+
+  
+</template>
+  
+<script type="text/ecmascript-6">
+import Swiper from 'swiper' 
+import 'swiper/css/swiper.css'
+export default {
+  data() {
+    return {
+      arrItem:[
+					{
+							name:'swiperSlide5',
+							imgUrl:'https://img02.hua.com/zhuanti/thanksday/2019/pc_thanks_banner.png'
+					},
+					{
+							name:'swiperSlide1',
+							imgUrl:'https://img02.hua.com/zhuanti/birthday/20181025/pc_banner.png'
+					},
+					{
+							name:'swiperSlide51',
+							imgUrl:'https://img02.hua.com/zhuanti/thanksday/2019/pc_thanks_banner.png'
+					},
+					{
+							name:'swiperSlide1111115',
+							imgUrl:'https://img02.hua.com/zhuanti/birthday/20181025/pc_banner.png'
+					}
+			]
+    }
+  },
+    mounted:function(){
+		new Swiper('.swiper-container', {
+			loop: true, // 循环模式选项
+			// 如果需要分页器
+			pagination: {
+				el: '.swiper-pagination',
+			},
+		})
+	},
+};
+</script>
+  
+<style lang="stylus" rel="stylesheet/stylus">
+    .banner
+        width 100%
+        height 400px
+        // overflow hidden
+        .swiper-container
+            .swiper-wrapper
+                .swiper-slide
+                    .img
+                        width 100%
+                        height 400px
+
+</style>
