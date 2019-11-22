adf40d2f1c09aa1f7c36bd0abb416047e1fa2a9e
tsstnt@163.com <52000212+tsstnt@users.noreply.github.com>
2019-11-22 22:43:30 +0800

    done

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="banner"> 
+  
     <div class="swiper-container">
       <div class="swiper-wrapper">
         <div class="swiper-slide" v-for="(el, index) in arrItem" :key="index">
@@ -9,7 +9,7 @@
       <!-- 如果需要分页器 -->
       <div class="swiper-pagination"></div>
     </div>
-	</div>
+  
 </template>
   
 <script type="text/ecmascript-6">
@@ -20,12 +20,12 @@ export default {
     return {
       arrItem:[
 					{
-							name:'swiperSlide1',
-							imgUrl:'https://img02.hua.com/zhuanti/birthday/20181025/pc_banner.png'
-					},
-					{
 							name:'swiperSlide5',
 							imgUrl:'https://img02.hua.com/zhuanti/thanksday/2019/pc_thanks_banner.png'
+					},
+					{
+							name:'swiperSlide1',
+							imgUrl:'https://img02.hua.com/zhuanti/birthday/20181025/pc_banner.png'
 					},
 					{
 							name:'swiperSlide51',
@@ -50,15 +50,4 @@ export default {
 };
 </script>
   
-<style lang="stylus" rel="stylesheet/stylus">
-.banner
-	width 100%
-	height 400px
-	// overflow hidden
-	.swiper-container
-		.swiper-wrapper
-			.swiper-slide
-				.img 
-					width 100%
-					height 400px
-</style>
+<style lang="stylus" rel="stylesheet/stylus"></style>
