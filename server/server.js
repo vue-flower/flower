c24d2b2ae423c49ac2012e4f1778648358595aef
wanglin <2842979502@qq.com>
2019-11-23 11:37:59 +0800

    第三天提交

---
+++ server/server.js
@@ -0,0 +1,16 @@
+let express = require('express')
+
+let flower = require('./datas/flower')
+let app = new express()
+
+app.get('/login',(req,res) => {})
+
+// let flower = require('./datas/flower')
+app.get('/flowerList',(req,res) => {
+  res.send(flower)
+})
+
+
+app.listen('3000',() => {
+  console.log('成功')
+})
